900de3752eac943ea8586219c55c831079aa2fa0
noahrepublic <noahbear08@hotmail.com>
2022-06-27 18:21:10 -0300

    Updated Server Code

--- src/Server/init.server.lua
+++ src/Server/init.server.lua
@@ -18,5 +18,10 @@ local template = {
 Data:LoadDataStore("DataServiceTest_1", template)
 
 game.Players.PlayerAdded:Connect(function(player)
-    Data:LoadData("Player_"..player.UserId)
+	local profile = Data:LoadData("Player_"..player.UserId, "forceload")
+	profile:ListenToRelease(function()
+		print("PROFILE RELEASED")
+	end)
+	profile.Data.Hi = 2
+	print(profile)
 end)
