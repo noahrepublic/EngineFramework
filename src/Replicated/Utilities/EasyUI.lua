9abb8b74371debacbd767ccb7c6bd84440486eea
Noah <noahdevelopsthings@gmail.com>
2022-07-03 23:36:10 -0300

    started writing

---
+++ src/Replicated/Utilities/EasyUI.lua
@@ -0,0 +1,61 @@
+-- @noahrepublic
+-- @version 1.0
+-- @date 07/02/22
+
+--  Variables --
+
+-- Public:
+
+local EasyUI = {}
+EasyUI.__index = EasyUI
+
+local Components = {}
+Components.__index = Components
+
+-- Private:
+
+local KnownProperties = {
+   Size = true,
+   Position = true,
+   AnchorPosition = true,
+   Visible = true,
+    
+}
+
+-- Functions --
+
+function EasyUI.new(name) -- Called for new GUI component
+   return setmetatable({
+      _Children = {}, -- [component] = {components}
+      _Build = nil,
+      _Name = name
+   },Components[name])
+end
+
+function Components:CreateComponent(component_type, name) -- i would add type checking however i am on mobile so it fills my screen and looks ugly, just like this comment
+   local component = setmetatable({
+      _Children = {},
+      _Build = component_type
+      _Name = name,
+      _Parent = self
+   }, Components[self._Children][name])
+   self._Children[name] = component
+   return component
+end
+
+function Components:Build()
+   if type(self._Build) == "string" and type(self._Parent._Build) ~= "string" then
+      -- build
+      
+      local err, success = pcall(Instance.new, self._Build)
+      if not success then
+         error("EasyUI | "..err)
+      end
+   elseif type(self._Parent._Build) == "string" then
+      error("EasyUI | You need to build the parent object first!)
+   else
+      warn("EasyUI | This object is already built!
+   end
+end
+
+return EasyUI
