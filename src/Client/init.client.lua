342e522776871a2b342fbd9689f8d1eda7b0b573
Noah <noahdevelopsthings@gmail.com>
2022-07-15 22:18:29 -0300

    Testing for Timers

--- src/Client/init.client.lua
+++ src/Client/init.client.lua
@@ -1,1 +1,15 @@
-print("Hello world, from client!")
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local TimeFlder = ReplicatedStorage:FindFirstChild("Common"):FindFirstChild("Utilities").Time
+
+local sync_request = TimeFlder:WaitForChild("TimeSyncRequest")
+local delay_request = TimeFlder:WaitForChild("TimeDelayRequest")
+
+local TimeSync = require(TimeFlder:FindFirstChild("TimeSync"))
+local timer = TimeSync:Sync()
+print(timer)
+
+timer.Synced:Connect(function()
+	print("SYNCED TIME!", timer.offset, "PING: ", timer.network_interval * 2000)
+end)
