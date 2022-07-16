64927a303f4e5aa1c06d71197c9789d06807d42c
Noah <noahdevelopsthings@gmail.com>
2022-07-15 22:18:23 -0300

    Removed features, so all libraries can stand alone

--- src/Replicated/Engine.lua
+++ src/Replicated/Engine.lua
@@ -3,8 +3,8 @@
 
 local Engine = {
 	Utilities = script.Parent.Utilities,
-	StartUp = script.Parent.StartUp:GetChildren(),
-	Services = script.Parent.Services:GetChildren(),
+	StartUp = script.Parent.StartUp,
+	Services = script.Parent.Services,
 	LoadedServices = {}
 }
 Engine.__index = Engine
@@ -13,30 +13,17 @@ local Utilities = script.Parent.Utilities
 
 -- Functions --
 
-function Engine.Start()
-	for _, v in pairs(Engine.StartUp) do
-		local success, msg = pcall(function()
-			require(v)
-		end)
-		if not success then
-			warn("Error loading " .. v.Name .. ": " .. msg)
-		else
-			print("Loaded " .. v.Name)
-		end
-	end
-
-	for _, v in pairs(Engine.Services) do
-		local service = require(v)
-		Engine.LoadedServices[v] = service
-	end
-end
-
 function Engine.Require(name)
-	if Utilities[name] then
+	if Utilities:FindFirstChild(name) then
 		return require(Utilities[name])
-	elseif Engine.StartUp[name] then
+	elseif Engine.Services:FindFirstChild(name) then
 		return require(Engine.StartUp[name])
 	else
+		for _, module in pairs(Utilities:GetDescendants()) do
+			if module.Name == name then
+				return require(module)
+			end
+		end
 		warn("Engine.Require: " .. name .. " not found.")
 	end
 end
