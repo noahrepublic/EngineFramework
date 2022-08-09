245dc684a973e51ca05e7438ec7bfa0f1163dfea
Noah <noahbear08@hotmail.com>
2022-08-09 14:46:21 -0300

    Update Soap.lua

--- src/Replicated/Utilities/Soap.lua
+++ src/Replicated/Utilities/Soap.lua
@@ -15,51 +15,51 @@ Soap.__index = Soap
 -- Functions --
 
 function Soap.new() -- init
-    return setmetatable({
-        _Tasks = {
-            _High = {},
-            _Default = {}
-        }
-    }, Soap)
+	return setmetatable({
+		_Tasks = {
+			_High = {},
+			_Default = {}
+		}
+	}, Soap)
 end
 
-function Soap:Add(task, priority)
-    local tasks = self._Tasks
-    if priority == "High" then priority = tasks._High else priority = tasks._Default end
-    -- temporary, later i will use smart insert once i make it
-    table.insert(priority, task)
-    if priority == tasks._High then
-        Soap:Add(task.spawn(self:Scrub(true)))
-    end
-    return task
+function Soap:Add(job, priority)
+	local tasks = self._Tasks
+	if priority == "High" then priority = tasks._High else priority = tasks._Default end
+	-- temporary, later i will use smart insert once i make it
+	table.insert(priority, job)
+	if priority == tasks._High then
+		Soap:Add(task.spawn(self:Scrub(true)))
+	end
+	return job
 end
 
 function Soap:Scrub(r)
-    local tasks = self._Tasks._Default
-    if r then
-        tasks = self._Tasks._High
-    end
-    
-    for index, step in pairs(tasks) do
-        if typeof(step) == "RBXScriptConnection" then
-            step:Disconnect()
-        elseif step:IsA("Tween") then
-            step:Stop()
-        elseif type(step) == "function" then
-            step()
-        elseif typeof(step) == "Instance" then
-            step:Destroy()
-        elseif type(step) == "thread" then
-            task.cancel(step)
-        elseif step.Destroy then
-            step:Destroy() -- custom modules can be used
-        elseif type(step) == "table" then
-            for i, _ in pairs(step) do
-                step[i] = nil
-            end
-        end
-        tasks[index] = nil
-    end
+	local tasks = self._Tasks._Default
+	if r then
+		tasks = self._Tasks._High
+	end
+
+	for index, step in pairs(tasks) do
+		if typeof(step) == "RBXScriptConnection" then
+			step:Disconnect()
+		elseif step:IsA("Tween") then
+			step:Stop()
+		elseif type(step) == "function" then
+			step()
+		elseif typeof(step) == "Instance" then
+			step:Destroy()
+		elseif type(step) == "thread" then
+			task.cancel(step)
+		elseif step.Destroy then
+			step:Destroy() -- custom modules can be used
+		elseif type(step) == "table" then
+			for i, _ in pairs(step) do
+				step[i] = nil
+			end
+		end
+		tasks[index] = nil
+	end
 end
 
 return Soap
